956663c1ff48c814abaa8b0198faf4c6a5f0a81e
lanzuln <lanzu8006@gmail.com>
2023-12-04 13:18:50 +0600

    product table, product multi image table , product sidebar navigate

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar-wrapper" data-simplebar="true">
     <div class="sidebar-header">
         <div>
-            <img src="{{asset('backendAdmin/assets/images/logo-icon.png')}}" class="logo-icon" alt="logo icon">
+            <img src="{{ asset('backendAdmin/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
         </div>
         <div>
             <h4 class="logo-text">Rukada</h4>
@@ -18,7 +18,7 @@
                 <div class="menu-title">Dashboard</div>
             </a>
             <ul>
-                <li> <a href="{{route('admin.dashboard')}}"><i class="bx bx-right-arrow-alt"></i>Default</a>
+                <li> <a href="{{ route('admin.dashboard') }}"><i class="bx bx-right-arrow-alt"></i>Default</a>
                 </li>
             </ul>
         </li>
@@ -30,8 +30,8 @@
                 <div class="menu-title">Brand</div>
             </a>
             <ul>
-                <li> <a href="{{route('all.brand')}}"><i class="bx bx-right-arrow-alt"></i>All brand</a></li>
-                <li> <a href="{{route('create.brand')}}"><i class="bx bx-right-arrow-alt"></i>Add brand</a></li>
+                <li> <a href="{{ route('all.brand') }}"><i class="bx bx-right-arrow-alt"></i>All brand</a></li>
+                <li> <a href="{{ route('create.brand') }}"><i class="bx bx-right-arrow-alt"></i>Add brand</a></li>
             </ul>
         </li>
         {{-- category --}}
@@ -42,34 +42,49 @@
                 <div class="menu-title">Category</div>
             </a>
             <ul>
-                <li> <a href="{{route('all.category')}}"><i class="bx bx-right-arrow-alt"></i>All category</a></li>
-                <li> <a href="{{route('create.category')}}"><i class="bx bx-right-arrow-alt"></i>Add category</a></li>
+                <li> <a href="{{ route('all.category') }}"><i class="bx bx-right-arrow-alt"></i>All category</a></li>
+                <li> <a href="{{ route('create.category') }}"><i class="bx bx-right-arrow-alt"></i>Add category</a></li>
             </ul>
         </li>
-         {{-- sub category --}}
-         <li>
+        {{-- sub category --}}
+        <li>
             <a href="javascript:;" class="has-arrow">
                 <div class="parent-icon"><i class="bx bx-category"></i>
                 </div>
                 <div class="menu-title">Sub Category</div>
             </a>
             <ul>
-                <li> <a href="{{route('all.subcategory')}}"><i class="bx bx-right-arrow-alt"></i>All category</a></li>
-                <li> <a href="{{route('create.subcategory')}}"><i class="bx bx-right-arrow-alt"></i>Add category</a></li>
+                <li> <a href="{{ route('all.subcategory') }}"><i class="bx bx-right-arrow-alt"></i>All category</a></li>
+                <li> <a href="{{ route('create.subcategory') }}"><i class="bx bx-right-arrow-alt"></i>Add category</a>
+                </li>
             </ul>
         </li>
-         {{--  --}}
-         <li class="menu-label">Vendor management</li>
+        {{-- product --}}
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="bx bx-category"></i>
+                </div>
+                <div class="menu-title">Product</div>
+            </a>
+            <ul>
+                <li> <a href=""><i class="bx bx-right-arrow-alt"></i>All Product</a></li>
+                <li> <a href=""><i class="bx bx-right-arrow-alt"></i>Add Product</a>
+                </li>
+            </ul>
+        </li>
+        {{--  --}}
+        <li class="menu-label">Vendor management</li>
 
-         <li>
+        <li>
             <a href="javascript:;" class="has-arrow">
                 <div class="parent-icon"><i class="bx bx-category"></i>
                 </div>
                 <div class="menu-title">Vendor management</div>
             </a>
             <ul>
-                <li> <a href="{{route('inactive.vendor')}}"><i class="bx bx-right-arrow-alt"></i>Inactive vendor</a></li>
-                <li> <a href="{{route('active.vendor')}}"><i class="bx bx-right-arrow-alt"></i>Active vendor</a></li>
+                <li> <a href="{{ route('inactive.vendor') }}"><i class="bx bx-right-arrow-alt"></i>Inactive vendor</a>
+                </li>
+                <li> <a href="{{ route('active.vendor') }}"><i class="bx bx-right-arrow-alt"></i>Active vendor</a></li>
             </ul>
         </li>
         <li class="menu-label">UI Elements</li>
